b4817bc300d5e6c6f1ea430f67a40ee35e6d6625
rkadam01 <rkadam01@xento.com>
2020-07-13 12:27:49 +0530

    first commit

--- src/multiplication.php
+++ src/multiplication.php
@@ -7,6 +7,7 @@ namespace rkadam01\Multiplication;
 class Multiplication
 {
 	public function getMultiplication( $data ) {
+		echo "here";
 		$firstElement = array_shift($data);
 		if( false == ctype_digit(implode('',$data))){
 			return 'Please enter the valid parameters.';
